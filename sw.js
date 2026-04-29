a3566cf223022a88001cfa24d664c9061b8a8263
nhess80 <nhess80@gmail.com>
2026-04-29 10:29:10 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lap-splitter-v1';
+const CACHE_NAME = 'lap-splitter-v2';
 const ASSETS = [
   '/',
   '/index.html',
@@ -25,14 +25,16 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request).then((response) => {
-        if (response.status === 200) {
-          const clone = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
-        }
-        return response;
+    fetch(event.request).then((response) => {
+      if (response.status === 200) {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+      }
+      return response;
+    }).catch(() => {
+      return caches.match(event.request).then((cached) => {
+        return cached || caches.match('/index.html');
       });
-    }).catch(() => caches.match('/index.html'))
+    })
   );
 });
